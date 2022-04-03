ad564744973e0f2437d5119e3ae05ec9f3f1d4f9
kadekframa <dframa19@gmail.com>
2022-04-03 23:18:47 +0800

    AJAX using XHR. Membuat Delete Request Book using WEB API support by dicoding. Semangat !!!

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -79,7 +79,28 @@ function main() {
     };
 
     const removeBook = (bookId) => {
-        // tuliskan kode di sini!
+        // Membuat instance dari XMLHttpRequest.
+        const xhr = new XMLHttpRequest();
+
+        // Menetapkan callback jika response sukses dan error.
+        xhr.onload = function(){
+            const responseJson = JSON.parse(this.responseText);
+            showResponseMessage(responseJson.message);
+            getBook();
+        }
+
+        xhr.onerror = function(){
+            showResponseMessage();
+        }
+
+        // Membuat DELETE request dan mentapkan target URL.
+        xhr.open("DELETE", `${baseUrl}/delete/${bookId}`);
+
+        // Menetapkan properti Content-Type dan X-Auth-Token pada Header request.
+        xhr.setRequestHeader("X-Auth-Token", "12345");
+
+        // Mengirimkan request.
+        xhr.send();
     };
 
 
